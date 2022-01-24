05c44f565b14ca40e25e0d6cdf242231f400b874
+ <+>
2022-01-24 10:15:19 -0500

    commit tree 19fea57374decce58b036d44b9b6ba3dc67a95ea

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,3 +2,11 @@ require 'nokogiri'
 require 'open-uri'
 
 html = open("https://flatironschool.com/")
+doc = Nokogiri::HTML(html)
+puts doc.css(".heading-financier").text.strip
+
+courses = doc.css(".card-feature")
+courses.each do |course|
+    puts course.text.strip
+end
+p doc.css(".card-feature")[0].attributes
